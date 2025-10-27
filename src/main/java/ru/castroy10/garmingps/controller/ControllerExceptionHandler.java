e3c4c6577aebaff90c87212e14e7de10ce7ba7de
Oleg <castroy10@yandex.ru>
2025-10-27 21:15:30 +0300

    Added application version to footer, handled basic error scenarios

--- src/main/java/ru/castroy10/garmingps/controller/ControllerExceptionHandler.java
+++ src/main/java/ru/castroy10/garmingps/controller/ControllerExceptionHandler.java
@@ -5,10 +5,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<?> noResourceFoundException(final NoResourceFoundException exception, final WebRequest request) {
+        final String path = request.getDescription(false).substring(4);
+        if (path.equals("/favicon.ico")) {
+            return ResponseEntity.notFound().build();
+        }
+        log.error("Resource not found: ", exception);
+        return ResponseEntity.notFound().build();
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> exception(final Exception exception) {
--- src/main/java/ru/castroy10/garmingps/controller/MainController.java
+++ src/main/java/ru/castroy10/garmingps/controller/MainController.java
@@ -2,8 +2,10 @@ package ru.castroy10.garmingps.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,8 +18,12 @@ public class MainController {
 
     private final GpsCreatorService gpsCreatorService;
 
+    @Value("${spring.application.version}")
+    private String version;
+
     @GetMapping()
-    public String index() {
+    public String index(final Model model) {
+        model.addAttribute("version", version);
         return "index";
     }
 
